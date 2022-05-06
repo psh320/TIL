c246e0a24daadffbe67bb247f26e22a6074fa7fe
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-05-06 16:36:15 +0900

    use Faker in ts

--- react-native/RNCourse/App.tsx
+++ react-native/RNCourse/App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as D from './src/data';
 
+const person = D.createRandomPerson()
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text>{JSON.stringify(person, null,2)}</Text>
       <StatusBar style="auto" />
     </View>
   );
