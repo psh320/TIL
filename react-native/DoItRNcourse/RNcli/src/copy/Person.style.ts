36ecf60657d6c4b83641896d2c36a6e1095a9d94
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-05-12 22:06:56 +0900

    Added styles for Person

--- react-native/DoItRNcourse/RNcli/src/copy/Person.style.ts
+++ react-native/DoItRNcourse/RNcli/src/copy/Person.style.ts
@@ -1,4 +1,20 @@
 import {StyleSheet} from 'react-native';
 import {Colors} from 'react-native-paper';
 
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  view: {backgroundColor: Colors.lime100, padding: 5},
+  avatar: {width: 50, height: 50, borderRadius: 25},
+  nameEmailView: {flexDirection: 'row', alignItems: 'center'},
+  name: {marginRight: 5, fontSize: 22, fontWeight: '500'},
+  email: {},
+  dateView: {},
+  createdDate: {},
+  text: {},
+  image: {width: '100%', height: 150},
+  countsView: {
+    flexDirection: 'row',
+    padding: 3,
+    justifyContent: 'space-around',
+  },
+  counts: {},
+});
--- react-native/DoItRNcourse/RNcli/src/copy/Person.tsx
+++ react-native/DoItRNcourse/RNcli/src/copy/Person.tsx
@@ -1,14 +1,38 @@
-import React, {FC} from 'react';
-import {Text} from 'react-native';
+import React from 'react';
+import type {FC} from 'react';
+import {Image, Text, View} from 'react-native';
 import * as D from '../data';
 import {styles} from './Person.style';
+import moment from 'moment-with-locales-es6';
+
+moment.locale('ko');
 
 export type PersonProps = {
   person: D.IPerson;
 };
 
 const Person: FC<PersonProps> = ({person}) => {
-  return <Text>{JSON.stringify(person, null, 2)}</Text>;
+  return (
+    <View style={[styles.view]}>
+      <Image source={{uri: person.avatar}} style={[styles.avatar]} />
+      <View>
+        <Text style={[styles.name]}>{person.name}</Text>
+        <Text style={[styles.email]}>{person.email}</Text>
+      </View>
+      <View style={[styles.dateView]}>
+        <Text>
+          {moment.(person.createdDate).startOf('day').fromNow()}
+        </Text>
+      </View>
+      <Text style={[styles.text]}>{person.comment}</Text>
+      <Image style={[styles.image]} source={{uri: person.image}} />
+      <View style={[styles.countsView]}>
+        <Text style={[styles.counts]}>{person.counts.comment}</Text>
+        <Text style={[styles.counts]}>{person.counts.retweet}</Text>
+        <Text style={[styles.counts]}>{person.counts.heart}</Text>
+      </View>
+    </View>
+  );
 };
 
 export default Person;
