e1b95fc750846dab302f15aff467f49cf61cc08d
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-05-14 00:56:21 +0900

    Added ComponentProps

--- react-native/DoItRNcourse/RNcli/src/components/TouchableView.tsx
+++ react-native/DoItRNcourse/RNcli/src/components/TouchableView.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import type {FC, ReactNode} from 'react';
 import {TouchableOpacity, View} from 'react-native';
+import type {ComponentProps} from 'react';
 
 export type TouchableViewProps = {
   children?: ReactNode;
