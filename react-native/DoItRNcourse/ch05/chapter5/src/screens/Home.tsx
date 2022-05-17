6991e675bad90075064118341f3e7750b9f1b0a9
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-05-17 14:20:22 +0900

    using Context

---
+++ react-native/DoItRNcourse/ch05/chapter5/src/screens/Home.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import {useTheme} from 'react-native-paper';
+
+const Home = () => {
+  const theme = useTheme();
+  const {fonts, colors} = theme;
+  const title = 'Home';
+  return (
+    <View style={[styles.view, {backgroundColor: colors.background}]}>
+      <View style={[styles.bar, {backgroundColor: colors.primary}]}>
+        <Text style={(styles.text, {color: colors.text}, fonts.medium)}>
+          TopBar
+        </Text>
+      </View>
+      <View style={styles.content}>
+        <Text style={[styles.text, {color: colors.text}, fonts.regular]}>
+          Welcome to Context World!
+        </Text>
+      </View>
+      <View style={[styles.bar, {backgroundColor: colors.accent}]}>
+        <Text style={(styles.text, {color: colors.text}, fonts.light)}>
+          BottomBar
+        </Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {flex: 1},
+  bar: {height: 50, flexDirection: 'row', padding: 5, alignItems: 'center'},
+  content: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  text: {fontSize: 20, textAlign: 'center'},
+});
+
+export default Home;
