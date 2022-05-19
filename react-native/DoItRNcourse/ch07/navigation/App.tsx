b2fd4cc1092756e7e11cf50f7b29e31434e82f09
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-05-19 18:45:13 +0900

    Adding custom nav components

--- react-native/DoItRNcourse/ch07/navigation/App.tsx
+++ react-native/DoItRNcourse/ch07/navigation/App.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import 'react-native-gesture-handler';
 import {enableScreens} from 'react-native-screens';
-import {NavigationContainer} from '@react-navigation/native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
+import {AppearanceProvider, useColorScheme} from 'react-native-appearance';
 
+import {ToggleThemeProvider} from './src//contexts';
 import MainNavigator from './src/screens/MainNavigator';
 
 enableScreens();
 
 export default function App() {
+  const scheme = useColorScheme(); //dark or light
+  const [theme, setTheme] = useState(
+    scheme === 'dark' ? DarkTheme : DefaultTheme,
+  );
+  const toggleTheme = useCallback(() => {
+    setTheme(({dark}) => (dark ? DefaultTheme : DarkTheme));
+  }, []);
+
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <MainNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <AppearanceProvider>
+      <ToggleThemeProvider toggleTheme={toggleTheme}>
+        <SafeAreaProvider>
+          <NavigationContainer theme={theme}>
+            <MainNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ToggleThemeProvider>
+    </AppearanceProvider>
   );
 }
--- react-native/DoItRNcourse/ch07/navigation/src/screens/MainNavigator.tsx
+++ react-native/DoItRNcourse/ch07/navigation/src/screens/MainNavigator.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, Text, View} from 'react-native';
+import {useTheme} from '@react-navigation/native';
+import {useToggleTheme} from '../contexts';
+import {Switch} from 'react-native-paper';
 
 export default function MainNavigator() {
+  const theme = useTheme();
+  const toggleTheme = useToggleTheme();
+
   return (
-    <View style={[styles.view]}>
-      <Text>This is top text.</Text>
-      <Text>This is Bottom text.</Text>
-    </View>
+    <SafeAreaView style={[styles.view]}>
+      <View style={[styles.view, {backgroundColor: theme.colors.background}]}>
+        <View style={[styles.topBar, {backgroundColor: theme.colors.primary}]}>
+          <Switch value={theme.dark} onValueChange={toggleTheme} />
+        </View>
+        <View style={[styles.view]}>
+          <Text style={[styles.text, {color: theme.colors.text}]}>
+            This is Top text.
+          </Text>
+          <Text style={[styles.text, {color: theme.colors.text}]}>
+            This is Bottom text.
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 const styles = StyleSheet.create({
   view: {flex: 1, alignItems: 'center', justifyContent: 'space-between'},
+  flex: {flex: 1},
+  topBar: {
+    width: '100%',
+    flexDirection: 'row',
+    padding: 5,
+    justifyContent: 'flex-end',
+  },
+  text: {fontSize: 20},
 });
