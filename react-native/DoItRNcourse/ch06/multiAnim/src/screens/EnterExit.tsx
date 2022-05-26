d965c70e6f02233a4aca14f5e67be675f5710224
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-05-26 19:58:01 +0900

    Complete ch6-3

--- react-native/DoItRNcourse/ch06/multiAnim/src/screens/EnterExit.tsx
+++ react-native/DoItRNcourse/ch06/multiAnim/src/screens/EnterExit.tsx
@@ -1,53 +1,53 @@
-import React, {useState, useCallback, useRef, useEffect} from 'react';
-import {Text, View, StyleSheet, Switch, FlatList} from 'react-native';
-import {useTheme} from 'react-native-paper';
-import {useToggleTheme} from '../contexts';
+import React, { useState, useCallback, useRef, useEffect } from 'react';
+import { Text, View, StyleSheet, Switch, FlatList } from 'react-native';
+import { useTheme } from 'react-native-paper';
+import { useToggleTheme } from '../contexts';
 import * as D from '../data';
-import Person from './Person';
+import Person from './PersonEnterExit';
 
 const People = () => {
   const [people, setPeople] = useState<D.IPerson[]>([D.createRandomPerson()]);
   const theme = useTheme();
   const toggleTheme = useToggleTheme();
   const addPerson = useCallback(() => {
-    setPeople(people => [D.createRandomPerson(), ...people]);
+    setPeople((people) => [D.createRandomPerson(), ...people]);
   }, []);
   const removeAllPersons = useCallback(() => {
-    setPeople(notUsed => []);
+    setPeople((notUsed) => []);
   }, []);
   const deletePerson = useCallback((id: string) => {
-    setPeople(people => people.filter(person => person.id !== id));
+    setPeople((people) => people.filter((person) => person.id !== id));
   }, []);
 
   useEffect(addPerson, []);
 
   return (
-    <View style={[styles.view, {backgroundColor: theme.colors.surface}]}>
-      <View style={[styles.topBar, {backgroundColor: theme.colors.accent}]}>
+    <View style={[styles.view, { backgroundColor: theme.colors.surface }]}>
+      <View style={[styles.topBar, { backgroundColor: theme.colors.accent }]}>
         <Text onPress={addPerson} style={styles.text}>
           add
         </Text>
         <Text onPress={removeAllPersons} style={styles.text}>
           remove all
         </Text>
-        <View style={{flex: 1}} />
+        <View style={{ flex: 1 }} />
         <Switch value={theme.dark} onValueChange={toggleTheme} />
       </View>
       <FlatList
         data={people}
-        renderItem={({item}) => (
+        renderItem={({ item }) => (
           <Person person={item} deletePressed={() => deletePerson(item.id)} />
         )}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  view: {flex: 1},
-  topBar: {flexDirection: 'row', padding: 5},
-  text: {marginRight: 10, fontSize: 20},
+  view: { flex: 1 },
+  topBar: { flexDirection: 'row', padding: 5 },
+  text: { marginRight: 10, fontSize: 20 },
 });
 
 export default People;
