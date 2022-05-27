fb70549f7fe2692d248359d2549cf544dc42a192
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-05-27 18:00:26 +0900

    Added src files

---
+++ react-native/DoItRNcourse/ch06/panResponder/src/hooks/useAnimatedValue.ts
@@ -0,0 +1,6 @@
+import React, {useRef} from 'react';
+import {Animated} from 'react-native';
+
+export const useAnimatedValue = (initValue: number = 0): Animated.Value => {
+  return useRef(new Animated.Value(initValue)).current;
+};
