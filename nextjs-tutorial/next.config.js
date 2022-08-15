60ec2017db56e311baaee17bb59107fc03ae19dd
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-08-15 20:37:03 +0900

    Add .env file

--- nextjs-tutorial/next.config.js
+++ nextjs-tutorial/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const API_KEY = "10923b261ba94d897ac6b81148314a3f";
+const API_KEY = process.env.API_KEY;
 
 const nextConfig = {
   reactStrictMode: true,
