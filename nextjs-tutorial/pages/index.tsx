a83123b6a01f7bad96b72319f318d0118e2e9e9e
Sungho Park <43461389+psh320@users.noreply.github.com>
2022-08-18 11:49:27 +0900

    add url masking

--- nextjs-tutorial/pages/index.tsx
+++ nextjs-tutorial/pages/index.tsx
@@ -1,22 +1,28 @@
+import { GetServerSideProps } from "next";
+import Link from "next/link";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
-export default function Home() {
-  const [movies, setMovies] = useState([]);
-  useEffect(() => {
-    (async () => {
-      const { results } = await (await fetch(`/api/movies`)).json();
-      console.log(results);
-      setMovies(results);
-    })();
-  }, []);
+export default function Home({ results }: any) {
+  const router = useRouter();
+  const onClick = (id: number, title: string) => {
+    router.push(
+      { pathname: `/movies/${id}`, query: { title } },
+      `/movies/${id}`
+    );
+  };
   return (
     <div className="container">
       <Seo title="Home" />
-      {!movies && <h4>Loading...!</h4>}
-      {movies?.map((movie) => (
-        <div className="movie" key={movie.id}>
-          <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+
+      {results?.map((movie) => (
+        <div
+          onClick={() => onClick(movie.id, movie.original_title)}
+          className="movie"
+          key={movie.id}
+        >
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
           <h4>{movie.original_title}</h4>
         </div>
       ))}
@@ -34,6 +40,9 @@ export default function Home() {
           transition: transform 0.2s ease-in-out;
           box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
         }
+        .movie {
+          cursor: pointer;
+        }
         .movie:hover img {
           transform: scale(1.05) translateY(-10px);
         }
@@ -45,3 +54,14 @@ export default function Home() {
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { results } = await (
+    await fetch(`http://localhost:3000/api/movies`)
+  ).json();
+  return {
+    props: {
+      results,
+    },
+  };
+};
